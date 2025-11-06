4c1b8bf0cb09f93c45856dddecfc5a617884a87f
VuVietLong1207 <khoalong1110@gmail.com>
2025-11-06 20:36:17 +0700

    Delete admission_management directory

--- admission_management/views/candidates/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include_once __DIR__ . '/../../controllers/CandidateController.php';
-
-// Controller sẽ xử lý xóa và chuyển hướng
-header('Location: ../../controllers/CandidateController.php?action=delete&id=' . $_GET['id']);
-exit();
-?>
